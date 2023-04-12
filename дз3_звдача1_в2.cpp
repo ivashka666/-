aab77a91e92272ddf80f25af625b328675557b29
ivashka666 <113200118+ivashka666@users.noreply.github.com>
2023-04-12 18:58:06 +0300

    Add files via upload

---
+++ дз3_звдача1_в2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+    int mas[] = { -2, -2, 0, 1, 1, 2, 4, 4, 5 };
+    int n = (sizeof(mas) / sizeof(*mas));
+    int  i = 0;
+    for (int x = 0; x < n; x++)
+        if (0 == count(mas, mas + i, mas[x]))
+            mas[i++] = mas[x];
+
+    for (int x = 0; x < i; x++)
+        cout << mas[x] << "  ";
+    cout << endl;
+    system("pause");
+    return 0;
+    //сложность О(n)
+} 
