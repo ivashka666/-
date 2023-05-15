9f0fd699a78c45b49da3f2ecf5944832747aef26
ivashka666 <113200118+ivashka666@users.noreply.github.com>
2023-05-15 13:35:28 +0300

    Add files via upload

---
+++ сортировка по столбцам.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>   
+#include <vector>     
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector < vector <int> > a(n, vector <int>(m));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        int ind = m;
+        int max = -20000;
+
+        for (int j = i; j < m; j++) {
+            int temp = 0;
+            for (int q = 0; q < n; q++) {
+                temp += a[q][j];
+            }
+            if (temp > max) {
+                max = temp;
+                ind = j;
+            }
+        }
+
+        for (int o = 0; o < n; o++) {
+            int temp;
+            temp = a[o][i];
+            a[o][i] = a[o][ind];
+            a[o][ind] = temp;
+        }
+    }
+
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << a[i][j] << ' ';
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ сортировка по строчкам.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>   
+#include <vector>     
+using namespace std;
+
+int main()
+{
+    int n1, m1, n2, m2;
+    cin >> n1 >> m1;
+    cin >> n2 >> m2;
+    if (m1 != n2) {
+        cout << "Error";
+        return 0;
+    }
+    vector < vector <int> > a(n1, vector <int>(m1));
+
+    for (int i = 0; i < n1; i++)
+        for (int j = 0; j < m1; j++)
+        {
+            cin >> a[i][j];
+        }
+
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = 0; j < m1; j++)
+            cout << a[i][j] << ' ';
+        cout << endl;
+    }
+    //
+    vector < vector <int> > b(n2, vector <int>(m2));
+    for (int i = 0; i < n2; i++)
+        for (int j = 0; j < m2; j++)
+        {
+            cin >> b[i][j];
+        }
+
+    for (int i = 0; i < n2; i++)
+    {
+        for (int j = 0; j < m2; j++)
+            cout << b[i][j] << ' ';
+        cout << endl;
+    }
+    vector < vector <int> > ppp(m1, vector <int>(n2));
+    for (int z = 0; z < n1; z++) {
+        for (int x = 0; x < m1; x++) {
+            int temp = 0;
+            for (int c = 0; c < m1; c++) {
+                temp += a[z][c] * b[c][x];
+            }
+            ppp[z][x] = temp;
+        }
+    }
+    for (int i = 0; i < n2; i++)
+    {
+        for (int j = 0; j < m2; j++)
+            cout << ppp[i][j] << ' ';
+        cout << endl;
+    }
+    return 0;
+}
