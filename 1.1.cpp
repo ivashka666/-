fab9b9684ba837d0911e79db72a4cca6a1b0e092
ivashka666 <113200118+ivashka666@users.noreply.github.com>
2023-05-20 01:20:29 +0300

    Add files via upload

---
+++ 1.1.cpp
@@ -0,0 +1,19 @@
+﻿struct Node {
+    int val;
+    Node* left = nullptr;
+    Node* right = nullptr;
+};
+
+bool search(int n, Node* root) {
+    if (root != nullptr) {
+        if (n == root->val) return true;
+        else {
+            bool left_side = search(n, root->left);
+            bool right_side = search(n, root->right);
+            if (left_side || right_side)
+                return true;
+            else return false;
+        }
+    }
+    return false;
+}
